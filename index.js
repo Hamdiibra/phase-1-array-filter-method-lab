2fbf9288ed4e169dfbc159674f8ce4890dff2fda
Hamdiibrahim <yusufhamdi137@gmail.com>
2024-09-30 12:40:53 +0300

    filter assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+function findMatching(driversArray,name) {
+    return driversArray.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+function fuzzyMatch(driversArray,name){
+    return driversArray.filter(driver => driver.toLowerCase().startsWith(name.toLowerCase()));
+}
+function matchName(driversArray,name){
+    return driversArray.filter(drivers => drivers.name.toLowerCase() === name.toLowerCase())
+}
